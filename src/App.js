8845f61aba6212a7c9300919023fa6e1d2363fd1
polanaeem1 <naeemros9@gmail.com>
2024-10-01 15:53:27 +0300

    home update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Home from './Home/Home';
 import SignUp from './SignUp/SignUp';
 import SignIn from './SingIn/SignIn';
 
 function App() {
   return (
     <>
+    <Home/>
     <SignUp/>
-    <SignIn/> 
+    <SignIn/>
     </>
   );
 }
