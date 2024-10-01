7453295688cb8e8e9660edc27c93c86ed0655c46
polanaeem1 <naeemros9@gmail.com>
2024-10-01 15:47:24 +0300

    Structure

---
+++ src/SingIn/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+// Create All the design with the states also like username and email 
+
+const SignIn = () => {
+  return (
+    <div>SignIn</div>
+  )
+}
+
+export default SignIn
