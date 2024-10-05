2ebdcf22f0f3304b041426bce80d71010d1713a0
MarvyNady <marvynady11@gmail.com>
2024-10-05 16:38:21 +0300

    home update

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ import Home from "./Home/Home";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <SignUp /> */}
-      <SignIn/>
-=======
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/SignIn" element={<SignIn />} />
       </Routes>
->>>>>>> 7b2d42f1d3f9b35f19c0af6752c51d50a0777a2b
     </div>
   );
 }
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
+
+export function Logo() {
+  return (
+    <div className="SignUp-logo">
+      <FontAwesomeIcon icon={faBolt} size="x" color="yellow" />
+      <h1>Passenger</h1>
+    </div>
+  );
+}
+
 
 const Home = () => {
   return (
     <div className='home-page'>
       <nav className='home-nav'>
-<i className="fa-solid fa-bolt-lightning"></i>
-
-⚡ Passenger
+        <Logo />
       </nav>
+      <div className='section-container'>
       <div className='home-c1'>Connect <br /><span className='home-grid'> Easily with</span> <br /> Anyone</div>
       <div className='home-c2'>
         <p>Stay connected with your friends,family effortlessly.
@@ -20,9 +31,8 @@ const Home = () => {
         <button className='home-log'>Log in   →</button>
         <button className='home-sign'>Sign up   →</button>
         </div>
-       
       </div>
-      <div class="lightning-bolt"></div>
+      </div>
     </div>
   )
 }
