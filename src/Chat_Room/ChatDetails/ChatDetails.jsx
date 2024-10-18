9ae2f2e7751d2dc864eee88dc0d8173a95ed0e04
MarvyNady <marvynady11@gmail.com>
2024-10-18 03:08:43 +0300

    chat details updates

--- src/Chat_Room/ChatDetails/ChatDetails.jsx
+++ src/Chat_Room/ChatDetails/ChatDetails.jsx
@@ -7,6 +7,7 @@ import { faThumbTack } from "@fortawesome/free-solid-svg-icons";
 import { faImages } from "@fortawesome/free-solid-svg-icons";
 import { faFileLines } from "@fortawesome/free-solid-svg-icons";
 import {faLink} from "@fortawesome/free-solid-svg-icons";
+import photo from "C:/Users/Infotech/chat app/Chat_App/src/images/profile p.png"
 const ChatDetails = () => {
   return (
     <div className='chat-details-container'>
@@ -19,20 +20,34 @@ const ChatDetails = () => {
       <FontAwesomeIcon className='icon' icon={faThumbTack} size="3x" color="#b8babc" />
       </div>
       <div className='c-photos'>
-      <FontAwesomeIcon icon={faImages} style={{color: "#c7c7c7",}} size='2x'/>
-        <span className='photos'>Photos</span> 
+      <FontAwesomeIcon icon={faImages} style={{color: "#c7c7c7",}} size='2x' className='photo-i'/>
+        <span className='photos'>Photos (1)</span> 
        <span className='see'>see all</span>
+       <div className='allPhotos'>
+        <img src={photo} alt="all photos" className='photo1'/>
+       </div>
          </div>
 
       <div className='c-files'>
-      <FontAwesomeIcon icon={faFileLines} style={{color: '#cdced0'}} size='2x' /> <span className='files'>Files</span>
+      <FontAwesomeIcon icon={faFileLines} style={{color: '#cdced0'}} size='2x' className='file-i'/> 
+      <span className='files'>Files (1)</span>
       <span className='see'>see all</span>
+      <div>
+      <iframe
+        src="./ECOM ASS.pdf"
+        width="100%"
+        height="50px"
+        title="PDF Viewer"
+      >ecom.pdf</iframe>
+      </div>
       </div>
 
       <div className='c-links'>
-      <FontAwesomeIcon icon= {faLink} style={{color: "#c7c7c7",}} size='2x'/>
-       <span className='links'>Links</span> 
+      <FontAwesomeIcon icon= {faLink} style={{color: "#c7c7c7",}} size='2x'
+      className='link-i'/>
+       <span className='links'>Links (1)</span> 
       <span className='see'>see all</span>
+      <a href="www.google.com">Google</a>
       </div>
     
     
